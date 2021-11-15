948c6323e05ca9d9027c04cf36bbb0e0a8bc8428
JayArrowz <arrowzflame321@gmail.com>
2021-11-15 10:27:59 +0000

    chore: add integration test

---
+++ src/Trakx.Shrimpy.ApiClient.Tests/Integration/HistoricalClientTests.cs.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Trakx.Shrimpy.ApiClient.Tests.Integration
+{
+    public class HistoricalClientTests : ShrimpyClientTestsBase
+    {
+        private readonly IHistoricalClient _historicalClient;
+
+        public HistoricalClientTests(ShrimpyApiFixture apiFixture, ITestOutputHelper output) : base(apiFixture, output)
+        {
+            _historicalClient = ServiceProvider.GetRequiredService<IHistoricalClient>();
+        }
+
+        [Fact]
+        public async Task GetHistoricalCandlesAsync_should_return_tickers()
+        {
+            var tickers = await _historicalClient.GetHistoricalCandlesAsync(Exchange.Binance,
+                "btc",
+                "usdc",
+                DateTimeOffset.Now,
+                DateTimeOffset.Now.AddMinutes(2),
+                10,
+                "1m");
+            tickers.Should().NotBeNull();
+        }
+    }
+}
