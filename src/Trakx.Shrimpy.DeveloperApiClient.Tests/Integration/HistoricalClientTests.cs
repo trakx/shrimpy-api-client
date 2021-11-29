7b719daea0a0fdd9fc7cd7386fb031c1fcc452a7
JayArrowz <49910176+JayArrowz@users.noreply.github.com>
2021-11-29 09:31:31 +0000

    feat: allow client to guard against failure from historical candles request (#10)
    * chore: do not retry historical endpoint if data is not avail
    
    * chore: modify retry logic
    
    * chore: block results with no data
    
    * chore: add comments and modify test name
    
    * chore: spelling mistake

--- src/Trakx.Shrimpy.DeveloperApiClient.Tests/Integration/HistoricalClientTests.cs
+++ src/Trakx.Shrimpy.DeveloperApiClient.Tests/Integration/HistoricalClientTests.cs
@@ -20,6 +20,17 @@ namespace Trakx.Shrimpy.DeveloperApiClient.Tests.Integration
         }
 
         [Fact]
+        public async Task GetHistoricalCandlesSafeAsync_should_block_results_with_no_data()
+        {
+            var start = DateTimeOffset.Parse("2021-01-01z");
+            var end = start.AddDays(2);
+            var candles = (await _historicalClient
+                .GetHistoricalCandlesSafeAsync(Exchange.Bittrex, "BTC", "USDC", start, end, 100, Interval._1d));
+            candles.Result.Should().BeEmpty();
+            candles.StatusCode.Should().Be(500);
+        }
+
+        [Fact]
         public async Task GetHistoricalCandlesAsync_should_return_results()
         {
             var start = DateTimeOffset.Parse("2021-01-01z");
